c562312d84ff4765c944ab3d7b20ed104a7d642c
Anatoliy <anatoliy.v@thrive.io>
2019-07-02 00:18:36 +0300

    first commit

---
+++ single_word_core_bot/Bot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace SingleWordBot
+{
+    public class Bot
+    {
+        readonly private ITelegramBotClient botClient;
+
+        public Bot()
+        {
+            botClient = new TelegramBotClient("895198692:AAGcsvPQogiNGxexv6rdpMC0XHej6nKJfM0");
+
+        }
+
+        public async Task BotMain (){
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            await Task.Delay(int.MaxValue);
+        }
+
+        async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
---
+++ single_word_core_bot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingleWordBot
+{
+    class Program
+    {
+        static Bot bot;
+
+
+        public static void Main(string[] args)
+        {
+            bot = new Bot();
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            await bot.BotMain();
+        }
+
+    }
+}
